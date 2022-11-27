5e6fe609987abe124765e4e13ca7041cd31059d2
vibeauratech <emmanuelakharia@gmail.com>
2022-11-27 05:28:50 +0100

    yes

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-gsap.from(".flex-two > *", 2,{
-  opacity:0,
-  delay:1,
-  stagger:0.2
-})
-
-gsap.from(".flex-one > *", 2,{
-  opacity:0,
-  delay:2.5,
-  stagger:0.2
-})
-
-gsap.from(".flex-three > *", 2,{
-  opacity:0,
-  delay:3,
-  stagger:0.2
-})
-
-gsap.from(".toggleBtn", 1,{
- x : -150,
- y : -150,
- delay : 3.5
-
-})
+$('.steeze-slider').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  speed: 1000
+ }); 
 
 
   
--- slick.js
+++ slick.js
@@ -1,9 +1,3 @@
-$('.steeze-slider').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    speed: 1000
-  }); 
-  
+
 
   
